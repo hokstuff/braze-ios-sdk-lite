d9159e858f81a9f8ca7d4c036dcdccac3623f66c
Daniel Hok <dhok35@gmail.com>
2021-02-05 18:01:03 -0500

    all files of 3.32.0 release

---
+++ Package-lowip.swift
@@ -0,0 +1,61 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+  name: "Appboy_iOS_SDK",
+  defaultLocalization: "en",
+  platforms: [
+    .iOS(.v9)
+  ],
+  products: [
+    .library(name: "AppboyKit", type: .static, targets: ["AppboyKit"]),
+    .library(name: "AppboyUI", targets: ["AppboyUI"]),
+    .library(name: "AppboyPushStory", targets: ["AppboyPushStory"])
+  ],
+  dependencies: [
+    .package(name: "SDWebImage", url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.8.2")
+  ],
+  targets: [
+    .binaryTarget(
+      name: "AppboyKitLibrary",
+      path: "AppboyKit/AppboyKitLibrary.xcframework"
+    ),
+    .target(
+      name: "AppboyKit",
+      dependencies: ["SDWebImage", "AppboyKitLibrary"],
+      path: "AppboyKit",
+      resources: [
+        .process("Appboy.bundle"),
+        .process("headers/AppboyKitLibrary/ZipArchive_LICENSE.txt")
+      ],
+      publicHeadersPath: "headers/AppboyKitLibrary",
+      linkerSettings: [
+        .linkedFramework("SystemConfiguration"),
+        .linkedFramework("QuartzCore"),
+        .linkedFramework("CoreImage"),
+        .linkedFramework("CoreText"),
+        .linkedFramework("WebKit"),
+        .linkedFramework("UserNotifications"),
+        .linkedFramework("StoreKit"),
+        .linkedFramework("CoreTelephony", .when(platforms: [.iOS])),
+        .linkedLibrary("z"),
+      ]
+    ),
+    .target(
+      name: "AppboyUI",
+      dependencies: ["AppboyKit"],
+      path: "AppboyUI",
+      resources: [
+        .process("ABKNewsFeed/Resources"),
+        .process("ABKInAppMessage/Resources"),
+        .process("ABKContentCards/Resources")
+      ],
+      publicHeadersPath: "include/AppboyUI"
+    ),
+    .binaryTarget(
+      name: "AppboyPushStory",
+      path: "Appboy-Push-Story/AppboyPushStory.xcframework"
+    )
+  ]
+)
