ba590fb246b737a53ec391191eefe98aad5bf816
Daniel Hok <dhok35@gmail.com>
2021-02-08 14:54:45 -0500

    update url and add Package.swift

---
+++ Package.swift
@@ -0,0 +1,63 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+  name: "Appboy_iOS_SDK",
+  defaultLocalization: "en",
+  platforms: [
+    .iOS(.v9)
+  ],
+  products: [
+    .library(name: "AppboyKit", type: .static, targets: ["AppboyKit"]),
+    .library(name: "AppboyUI", targets: ["AppboyUI"]),
+    .library(name: "AppboyPushStory", targets: ["AppboyPushStory"])
+  ],
+  dependencies: [
+    .package(name: "SDWebImage", url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.8.2")
+  ],
+  targets: [
+    .binaryTarget(
+      name: "AppboyKitLibrary",
+      url: "https://github.com/hokstuff/braze-ios-sdk-lite/releases/download/3.32.0/AppboyKitLibrary.xcframework.zip",
+      checksum: "372a625fd5b23afbc727587c37d69745b7984c010115731ec0bed5c430375961"
+    ),
+    .target(
+      name: "AppboyKit",
+      dependencies: ["SDWebImage", "AppboyKitLibrary"],
+      path: "AppboyKit",
+      resources: [
+        .process("Appboy.bundle"),
+        .process("headers/AppboyKitLibrary/ZipArchive_LICENSE.txt")
+      ],
+      publicHeadersPath: "headers/AppboyKitLibrary",
+      linkerSettings: [
+        .linkedFramework("SystemConfiguration"),
+        .linkedFramework("QuartzCore"),
+        .linkedFramework("CoreImage"),
+        .linkedFramework("CoreText"),
+        .linkedFramework("WebKit"),
+        .linkedFramework("UserNotifications"),
+        .linkedFramework("StoreKit"),
+        .linkedFramework("CoreTelephony", .when(platforms: [.iOS])),
+        .linkedLibrary("z"),
+      ]
+    ),
+    .target(
+      name: "AppboyUI",
+      dependencies: ["AppboyKit"],
+      path: "AppboyUI",
+      resources: [
+        .process("ABKNewsFeed/Resources"),
+        .process("ABKInAppMessage/Resources"),
+        .process("ABKContentCards/Resources")
+      ],
+      publicHeadersPath: "include/AppboyUI"
+    ),
+    .binaryTarget(
+      name: "AppboyPushStory",
+      url: "https://github.com/hokstuff/braze-ios-sdk-lite/releases/download/3.32.0/AppboyPushStory.xcframework.zip",
+      checksum: "608247ea566392ce189b7c72d1236bfd86f1dec8b2a3c4c18a6844f406aa7f87"
+    )
+  ]
+)
